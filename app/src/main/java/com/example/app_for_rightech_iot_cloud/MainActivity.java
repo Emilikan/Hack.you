4ea9fd332399cba22f2a24e1f74cd6771c7d4d2f
Emilikan <emilqiwi@gmail.com>
2019-05-23 16:04:39 +0300

    add toolbars

--- app/src/main/java/com/example/app_for_rightech_iot_cloud/MainActivity.java
+++ app/src/main/java/com/example/app_for_rightech_iot_cloud/MainActivity.java
@@ -1,24 +1,56 @@
 package com.example.app_for_rightech_iot_cloud;
 
-import android.graphics.Color;
-import android.support.design.widget.TabLayout;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Метровагонмаш");
+
         FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment = new Main();
+        Fragment fragment = new MainFragment();
         fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_notification) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            Fragment fragment = new Notifications();
+            fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+            getSupportActionBar().setTitle("Уведомления");
+        } else if (item.getItemId() == R.id.action_ai) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            Fragment fragment = new Neuronet();
+            fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+            getSupportActionBar().setTitle("Нейросеть");
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/app_for_rightech_iot_cloud/MainFragment.java
@@ -0,0 +1,103 @@
+package com.example.app_for_rightech_iot_cloud;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainFragment extends Fragment {
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        List<Fragment> fragmentList = new ArrayList<>();
+        List<String> fragmentTitles = new ArrayList<>();
+
+        ViewPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitles.get(position);
+        }
+
+        void addFragment(Fragment fragment, String name) {
+            fragmentList.add(fragment);
+            fragmentTitles.add(name);
+        }
+    }
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        viewPager = rootView.findViewById(R.id.viewpager);
+        tabLayout = rootView.findViewById(R.id.tabs);
+
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager());
+
+        viewPagerAdapter.addFragment(new Now(), "Состояние");
+        viewPagerAdapter.addFragment(new Hisrory(), "История");
+
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+}

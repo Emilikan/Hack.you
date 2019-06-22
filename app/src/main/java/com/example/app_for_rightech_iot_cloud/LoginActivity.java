e7cc1fb2ed41e4c50698bb0ef7fd2c813385ee9e
dmitriy0 <sevdmitriy0804@mail.ru>
2019-06-22 13:14:10 +0300

    dar theme login

--- app/src/main/java/com/example/app_for_rightech_iot_cloud/LoginActivity.java
+++ app/src/main/java/com/example/app_for_rightech_iot_cloud/LoginActivity.java
@@ -1,15 +1,27 @@
 package com.example.app_for_rightech_iot_cloud;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import org.w3c.dom.Text;
+
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -22,11 +34,42 @@ public class LoginActivity extends AppCompatActivity {
     private static ApiAuth apiAuth;
     private boolean resp;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
         Button btnSignIn = findViewById(R.id.confirm);
+        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (Objects.equals(preferences.getString("theme", "light"), "dark")){
+            setTheme(R.style.DarkTheme);
+            findViewById(R.id.linearLayout).setBackgroundColor(Color.parseColor("#18191D"));
+            EditText login = findViewById(R.id.Login);
+            EditText pass = findViewById(R.id.Password);
+            login.setBackgroundResource(R.drawable.input_dark);
+            pass.setBackgroundResource(R.drawable.input_dark);
+            login.setTextColor(Color.parseColor("#B7BEC7"));
+            pass.setTextColor(Color.parseColor("#B7BEC7"));
+            btnSignIn.setBackgroundResource(R.drawable.button_dark);
+            btnSignIn.setTextColor(Color.parseColor("#FFFFFF"));
+            ImageView logo =  findViewById(R.id.imageView4);
+            logo.setImageResource(R.drawable.logo_dark);
+        }
+        else{
+            setTheme(R.style.AppTheme);
+            findViewById(R.id.linearLayout).setBackgroundColor(Color.parseColor("#ed1a3a"));
+            EditText login = findViewById(R.id.Login);
+            EditText pass = findViewById(R.id.Password);
+            login.setBackgroundResource(R.drawable.input_primary);
+            pass.setBackgroundResource(R.drawable.input_primary);
+            login.setTextColor(Color.parseColor("#ffffff"));
+            pass.setTextColor(Color.parseColor("#ffffff"));
+            btnSignIn.setBackgroundResource(R.drawable.button_background);
+            btnSignIn.setTextColor(Color.parseColor("#ed1a3a"));
+            ImageView logo =  findViewById(R.id.imageView4);
+            logo.setImageResource(R.drawable.logo);
+        }
+
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

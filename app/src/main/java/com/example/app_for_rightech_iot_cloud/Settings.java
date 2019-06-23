3c45089987c76fb2cd1cf331a11244209b50c3e3
dmitriy0 <sevdmitriy0804@mail.ru>
2019-06-24 00:09:25 +0300

    settings

--- app/src/main/java/com/example/app_for_rightech_iot_cloud/Settings.java
+++ app/src/main/java/com/example/app_for_rightech_iot_cloud/Settings.java
@@ -14,6 +14,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import java.util.Objects;
 
@@ -29,16 +31,50 @@ public class Settings extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              final Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
-        final Button changeTheme = rootView.findViewById(R.id.button);
+        final Switch changeTheme = rootView.findViewById(R.id.switch1);
         final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
         final SharedPreferences.Editor editor = preferences.edit();
         if (Objects.equals(preferences.getString("theme", "light"), "dark")){
             rootView.findViewById(R.id.settingsLayout).setBackgroundColor(Color.parseColor("#18191D"));
-            changeTheme.setText("Светлая тема");
+            rootView.findViewById(R.id.layout1).setBackgroundResource(R.drawable.dark_recycler_view_frame);
+            rootView.findViewById(R.id.layout2).setBackgroundResource(R.drawable.dark_recycler_view_frame);
+            rootView.findViewById(R.id.layout3).setBackgroundResource(R.drawable.dark_recycler_view_frame);
+            TextView text = rootView.findViewById(R.id.textView);
+            text.setTextColor(Color.parseColor("#E9E9E9"));
+            TextView text2 = rootView.findViewById(R.id.textView5);
+            text2.setTextColor(Color.parseColor("#E9E9E9"));
+            TextView text4 = rootView.findViewById(R.id.textView7);
+            text4.setTextColor(Color.parseColor("#E9E9E9"));
+            TextView text5 = rootView.findViewById(R.id.textView2);
+            text5.setTextColor(Color.parseColor("#E9E9E9"));
+            TextView text6 = rootView.findViewById(R.id.textView3);
+            text6.setTextColor(Color.parseColor("#E9E9E9"));
+            TextView text7 = rootView.findViewById(R.id.textView12);
+            text7.setTextColor(Color.parseColor("#E9E9E9"));
+            TextView text9 = rootView.findViewById(R.id.textView14);
+            text9.setTextColor(Color.parseColor("#E9E9E9"));
+            changeTheme.setChecked(true);
         }
         else{
             rootView.findViewById(R.id.settingsLayout).setBackgroundColor(Color.parseColor("#ffffff"));
-            changeTheme.setText("Темная тема");
+            rootView.findViewById(R.id.layout1).setBackgroundResource(R.drawable.recycler_view_frame);
+            rootView.findViewById(R.id.layout2).setBackgroundResource(R.drawable.recycler_view_frame);
+            rootView.findViewById(R.id.layout3).setBackgroundResource(R.drawable.recycler_view_frame);
+            TextView text = rootView.findViewById(R.id.textView);
+            text.setTextColor(Color.parseColor("#18191D"));
+            TextView text2 = rootView.findViewById(R.id.textView5);
+            text2.setTextColor(Color.parseColor("#000000"));
+            TextView text4 = rootView.findViewById(R.id.textView7);
+            text4.setTextColor(Color.parseColor("#000000"));
+            TextView text5 = rootView.findViewById(R.id.textView2);
+            text5.setTextColor(Color.parseColor("#18191D"));
+            TextView text6 = rootView.findViewById(R.id.textView3);
+            text6.setTextColor(Color.parseColor("#18191D"));
+            TextView text7 = rootView.findViewById(R.id.textView12);
+            text7.setTextColor(Color.parseColor("#000000"));
+            TextView text9 = rootView.findViewById(R.id.textView14);
+            text9.setTextColor(Color.parseColor("#000000"));
+            changeTheme.setChecked(false);
         }
         changeTheme.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
